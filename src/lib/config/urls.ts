3b55a0763cf82bfe2e97af200d4ac9c0af14cf0c
Leander Muskalla <muskalla@sozialit.de>
2026-06-14 13:16:43 +0200

    Dynamic links for desktop/mobile

--- src/lib/config/urls.ts
+++ src/lib/config/urls.ts
@@ -1,3 +1,8 @@
+import { env } from '$env/dynamic/public';
+
+const dv = env.PUBLIC_DESKTOP_VERSION ?? '1.0.7';
+const mv = env.PUBLIC_MOBILE_VERSION ?? '1.0.7';
+
 export const urls = {
 	login: 'https://my.solyto.app/auth/login',
 	register: 'https://my.solyto.app/auth/register',
@@ -9,12 +14,13 @@ export const urls = {
 	discord: 'https://discord.gg/REyWQKgUcq',
 	releases: 'https://github.com/solyto/desktop/releases/latest',
 	downloads: {
-		windows: 'https://github.com/solyto/desktop/releases/download/v1.0.7/solyto-1.0.7.exe',
-		macos: 'https://github.com/solyto/desktop/releases/download/v1.0.7/solyto-1.0.7.dmg',
+		windows: `https://github.com/solyto/desktop/releases/download/v${dv}/solyto-${dv}.exe`,
+		macos: `https://github.com/solyto/desktop/releases/download/v${dv}/solyto-${dv}.dmg`,
 		linux: {
-			deb: 'https://github.com/solyto/desktop/releases/download/v1.0.7/solyto-1.0.7.deb',
-			rpm: 'https://github.com/solyto/desktop/releases/download/v1.0.7/solyto-1.0.7.rpm',
-			appimage: 'https://github.com/solyto/desktop/releases/download/v1.0.7/solyto-1.0.7.AppImage'
-		}
+			deb: `https://github.com/solyto/desktop/releases/download/v${dv}/solyto-${dv}.deb`,
+			rpm: `https://github.com/solyto/desktop/releases/download/v${dv}/solyto-${dv}.rpm`,
+			appimage: `https://github.com/solyto/desktop/releases/download/v${dv}/solyto-${dv}.AppImage`
+		},
+		android: `https://github.com/solyto/mobile/releases/download/v${mv}/app-release.apk`
 	}
-} as const;
+};
